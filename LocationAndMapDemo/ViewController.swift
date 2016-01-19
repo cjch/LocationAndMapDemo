25ac3815c39dcc5fcfdc5523bd4d64bc6b98e9c9
cjch <350765427@qq.com>
2016-01-19 16:47:50 +0800

    core location

--- LocationAndMapDemo/ViewController.swift
+++ LocationAndMapDemo/ViewController.swift
@@ -7,19 +7,70 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var horAccLabel: UILabel!
+    @IBOutlet weak var altitudeLabel: UILabel!
+    @IBOutlet weak var verAccLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    
+    var locationManager: CLLocationManager?
+    var startPoint: CLLocation?
+    var distanceFromStart: CLLocationDistance?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self;
+        locationManager?.desiredAccuracy = kCLLocationAccuracyBest
+        
+//        let status = CLLocationManager.authorizationStatus()
+        if UIDevice.currentDevice().systemVersion > "8" {
+            locationManager?.requestAlwaysAuthorization()
+        }
+        
+        locationManager?.startUpdatingLocation()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //MARK: CLLocationManagerDelegate
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let newLocation = locations.last
+        
+        latitudeLabel.text = String(format: "%g\u{00B0}", (newLocation?.coordinate.latitude)!)
+        longitudeLabel.text = String(format: "%g\u{00B0}", (newLocation?.coordinate.longitude)!)
+        horAccLabel.text = String(format: "%gm", (newLocation?.horizontalAccuracy)!)
+        
+        altitudeLabel.text = String(format: "%gm", (newLocation?.altitude)!)
+        verAccLabel.text = String(format: "%gm", (newLocation?.verticalAccuracy)!)
+        
+        if newLocation?.horizontalAccuracy < 0 || newLocation?.verticalAccuracy < 0 {
+            return;
+        }
+        
+        if newLocation?.horizontalAccuracy > 100 || newLocation?.verticalAccuracy > 50 {
+            return;
+        }
+        
+        if startPoint == nil {
+            startPoint = newLocation;
+            distanceFromStart = 0;
+        } else {
+            self.distanceFromStart = newLocation?.distanceFromLocation(startPoint!)
+        }
+        print(distanceFromStart)
+        distanceLabel.text = String(format: "%gm", distanceFromStart!);
     }
-
-
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print(error)
+        let message = error.code == CLError.Denied.rawValue ? "Access Denied" : error.description
+        let alert = UIAlertView(title: "error getting location", message: message, delegate: nil, cancelButtonTitle: "ok")
+        alert.show()
+    }
 }
 
---
+++ README.md
@@ -0,0 +1,4 @@
+# LocationMapDemo
+
+## Core Location
+应用定位服务: 在plist中配置NSLocationAlwaysUsageDescription 或 NSLocationWhenInUseUsageDescription，对应调用requestAlwaysAuthorization 或 requestWhenInUseAuthorization
